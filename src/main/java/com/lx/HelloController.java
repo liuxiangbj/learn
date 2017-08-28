14c23eba94dd9d4f4bed90387173bd16fd919335
liuxiang <liuxiang14@meituan.com>
2017-08-28 18:18:14 +0800

    add

---
+++ src/main/java/com/lx/HelloController.java
@@ -0,0 +1,25 @@
+package com.lx;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+
+/**
+ * Created by liuxiang14 on 2017/8/28.
+ */
+@Controller
+public class HelloController {
+    @Value("${application.message:Hello World}")
+    private String message = "Hello World";
+
+    @RequestMapping("/hello")
+    public String welcome(ModelMap model) {
+        model.put("time", new Date());
+        model.put("message", this.message);
+        return "hello";
+    }
+}
+
